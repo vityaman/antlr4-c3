cfa814b40952faeba50048ab1ddb32378c9df0ef
vityaman <vityaman.dev@yandex.ru>
2024-08-06 17:49:46 +0300

    #138 Prepare CMake to use an existing ANTLR

--- ports/cpp/cmake/FindAntlr4Cpp.cmake
+++ ports/cpp/cmake/FindAntlr4Cpp.cmake
@@ -1,6 +1,28 @@
-set(ANTLR4_WITH_STATIC_CRT OFF)
+set(ANTLR4_TAG 4.13.1)
 set(ANTLR4_ZIP_REPOSITORY https://github.com/antlr/antlr4/archive/refs/tags/${ANTLR4_TAG}.zip)
-add_definitions(-DANTLR4CPP_STATIC)
-include(ExternalAntlr4Cpp)
 
-set(ANTLR4C3_ANTLR4_STATIC antlr4_static)
+if(NOT TARGET antlr4_static)
+  set(ANTLR4_STATIC antlr4_static)
+
+  set(ANTLR4_WITH_STATIC_CRT OFF)
+  add_definitions(-DANTLR4CPP_STATIC)
+  include(ExternalAntlr4Cpp)
+
+else()
+  set(ANTLR4_STATIC existing_antlr4_static)
+
+  add_library(${ANTLR4_STATIC} STATIC IMPORTED)
+
+  add_dependencies(${ANTLR4_STATIC} antlr4_static)
+
+  set_target_properties(
+    ${ANTLR4_STATIC} PROPERTIES
+    IMPORTED_LOCATION ${ANTLR4_STATIC_LIBRARIES}
+  )
+
+  target_include_directories(
+    ${ANTLR4_STATIC}
+    INTERFACE ${ANTLR4_INCLUDE_DIRS}
+  )
+
+endif()
