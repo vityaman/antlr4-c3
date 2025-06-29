3734f5e4abc93e03b753a93035fbcd8a1c017acb
Viktor Smirnov <vitya-smirnov@yandex-team.com>
2025-06-29 14:24:26 +0300

    #138 Run collectCandidates

--- ports/cpp/test_package/conanfile.py
+++ ports/cpp/test_package/conanfile.py
@@ -12,8 +12,10 @@ class antlr4_c3TestConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
         self.requires("antlr4-cppruntime/4.13.1")
+        self.requires("antlr4/4.13.1")
 
     def build(self):
+
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
--- ports/cpp/test_package/src/example.cpp
+++ ports/cpp/test_package/src/example.cpp
@@ -1,3 +1,6 @@
+#include "expr/ExprParser.h"
+#include "expr/ExprLexer.h"
+
 #include "antlr4-runtime.h"
 #include "antlr4-c3.hpp"
 
@@ -5,5 +8,16 @@
 #include <string>
 
 int main() {
-    c3::CodeCompletionCore* core = nullptr;
+    const std::string input = "1 + 2 * 3";
+    antlr4::ANTLRInputStream input_stream(input);
+    ExprLexer lexer(&input_stream);
+    antlr4::CommonTokenStream tokens(&lexer);
+    ExprParser parser(&tokens);
+
+    parser.prog();
+    c3::CodeCompletionCore core(&parser);
+
+    c3::CandidatesCollection candidates = core.collectCandidates(0);
+
+    return 0;
 }
